cffced8a071f87ace5e10ef9ff623114dbfd79ca
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-10 18:21:29 +0530

    data saved

--- src/app/job-level-type-table/job-level-type-table.ts
+++ src/app/job-level-type-table/job-level-type-table.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../Shared/shared.module';
 import { Api } from '../../Services/api';
 import { Router } from '@angular/router';
+import { JobLevelTypeForm } from '../job-level-type-form/job-level-type-form';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-job-level-type-table',
-  imports: [SharedModule],
+  imports: [SharedModule,JobLevelTypeForm],
   templateUrl: './job-level-type-table.html',
   styleUrl: './job-level-type-table.css'
 })
 export class JobLevelTypeTable {
 jobLevelShift:any;
 
-constructor(public apiService: Api, public router: Router){
+constructor(public apiService: Api, public router: Router, public modalService: NgbModal ){
   
 }
 ngOnInit(){
@@ -25,7 +27,11 @@ ngOnInit(){
 }
 
 addDetails(){
-  console.log(1)
+  console.log(1);
   this.router.navigate(['/jobleveltypeform'])
+}
+
+openModal(modal: any){
+  this.modalService.open(modal,{centered: true, size: "lg", scrollable: true})
 }
 }

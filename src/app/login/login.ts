7d45aade2420b2be768e7db9064f4d08e208ba9b
Prasanna <prasannav2606@gmail.com>
2025-07-10 14:01:24 +0530

    Check

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../Shared/shared.module';
 import { Loader } from '../../Services/loader';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class Login {
   password:any;
 
 
-  constructor(public loaderService:Loader){
+  constructor(public loaderService:Loader,public router:Router){
 
   }
 
@@ -23,6 +24,7 @@ export class Login {
       if(this.userName == "Amaze-1234" && this.password == "Amaze-1234"){
          sessionStorage.setItem("isLogin",'Yes');
       this.loaderService.isLogin='Yes';
+      this.router.navigate(['/department-table']);
          }
        else{
       Swal.fire("Enter Valid data");

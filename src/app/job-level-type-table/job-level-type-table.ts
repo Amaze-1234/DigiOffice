ee442071c3900a8d0a8243c90ec0a2a351facba2
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-10 15:44:57 +0530

    data saved

--- src/app/job-level-type-table/job-level-type-table.ts
+++ src/app/job-level-type-table/job-level-type-table.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../Shared/shared.module';
 
 @Component({
   selector: 'app-job-level-type-table',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './job-level-type-table.html',
   styleUrl: './job-level-type-table.css'
 })
 export class JobLevelTypeTable {
-
+jobLevelShift:any;
 }

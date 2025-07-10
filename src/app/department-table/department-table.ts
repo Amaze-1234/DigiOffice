67e0c3562b9867a830fb0a5d3e7f4a3839720e37
chethanaAmaze <chethana@amazeinc.in>
2025-07-10 15:02:33 +0530

    taking data

--- src/app/department-table/department-table.ts
+++ src/app/department-table/department-table.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { Api } from '../../Services/api';
+import axios from 'axios';
 @Component({
   selector: 'app-department-table',
   imports: [CommonModule],
@@ -9,9 +10,17 @@ import { Component } from '@angular/core';
 })
 export class DepartmentTable {
 
+DepartmentData:any;
+constructor(public api:Api){}
+ngOnInit()
+{
 
+}
 
+async getDepartmentdetails()
+{
 
+}
 
 
 }

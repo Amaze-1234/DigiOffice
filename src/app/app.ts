7c15dcfbccc1735953eda4455a6fdc263522f3c9
Prasanna <prasannav2606@gmail.com>
2025-07-10 11:28:36 +0530

    Check

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from './side-bar/side-bar';
 import { Footer } from './footer/footer';
+import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SideBar,Footer],
+  imports: [RouterOutlet,SideBar,Footer,NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

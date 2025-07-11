7313b44f5e8ff29cfe4d16bcf4068c5984b75d7a
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-11 17:47:25 +0530

    data

--- src/app/province-form/province-form.ts
+++ src/app/province-form/province-form.ts
@@ -1,18 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Api } from '../../Services/api';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { SharedModule } from '../../Shared/shared.module';
 
 @Component({
   selector: 'app-province-form',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './province-form.html',
   styleUrl: './province-form.css'
 })
 export class ProvinceForm {
-  editid: any;
+  @Input() editid: any;
+  @Output() closeModal= new EventEmitter<any>();
   contactForm: any;
+   countryList:any=[
+    {
+      ID:1,
+      value:"India"
+    },
+    {
+      ID:2,
+      value:"Africa"
+    },
+    {
+      ID:3,
+      value:"Srilanka"
+    },
+    {
+      ID:4,
+      value:"France"
+    }]
+
   constructor(public api: Api,public router:Router,public activateRoute:ActivatedRoute ,public modalservice:NgbModal) {}
   ngOnInit() {
        if (this.editid) {
@@ -36,5 +57,34 @@ export class ProvinceForm {
       provinceDescription:new FormControl(response.data[0].provinceDescription,Validators.required),
     })
   }
+
+  async submit( type:any){
+      if (this.contactForm.invalid) {
+      Swal.fire({
+        text: 'Please fill all the details'
+      });
+      return;
+    }
+  
+      if (type == 'save') {
+        let result = await this.api.postMethod('Master/InsertProvince', this.contactForm.value);
+        if (result.data > 0) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Saved Successfully',
+          });
+        }
+      } else {
+        let result = await this.api.postMethod('Master/UpdateProvince', this.contactForm.value);
+        if (result.data > 0) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Updated Successfully',
+          });
+        }
+      }
+    }
+
+
 
 }
--- src/app/province-table/province-table.ts
+++ src/app/province-table/province-table.ts
@@ -1,11 +1,65 @@
 import { Component } from '@angular/core';
+import { ProvinceForm } from '../province-form/province-form';
+import Swal from 'sweetalert2';
+import { Api } from '../../Services/api';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from '../../Shared/shared.module';
 
 @Component({
   selector: 'app-province-table',
-  imports: [],
+  imports: [ProvinceForm, SharedModule],
   templateUrl: './province-table.html',
   styleUrl: './province-table.css'
 })
 export class ProvinceTable {
+  editid: any;
+  data: any;
+  provinceData: any;
+  constructor(public api:Api, public router:Router,public modalservice:NgbModal){}
+
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  async getData() {
+    const result = await this.api.getMethod("Master/GetProvince");
+    console.log( result.data);
+    this.provinceData = result.data;
+  }
+
+  openModal(Modal: any,id:any =null ){
+    if(id){
+      this.editid=id;
+    }   
+      this.modalservice.open(Modal,{centered: true, size:"lg", backdrop:'static'});
+  }
+  close(data:any=null){
+    this.editid = null;
+    this.modalservice.dismissAll();
+  }
+  async delete(id: any) {
+    const confirmation = await Swal.fire({
+      title: "Are you sure you want to delete?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes"
+    });
+    if (confirmation.isConfirmed) {
+      const result = await this.api.getMethod(`Master/DeleteProvince?ID=${id}`);
+      
+      if (result.data > 0) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+        this.getData();
+      }
+    }
+  }
 
 }

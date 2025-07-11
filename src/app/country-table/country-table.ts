3893922863737783c1bdfcfa1f2b5a7e499c1120
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-11 15:12:11 +0530

    country table

--- src/app/country-table/country-table.ts
+++ src/app/country-table/country-table.ts
@@ -3,16 +3,20 @@ import { CountryForm } from '../country-form/country-form';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { Api } from '../../Services/api';
+import { SharedModule } from '../../Shared/shared.module';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-country-table',
-  imports: [CountryForm],
+  imports: [CountryForm, SharedModule],
   templateUrl: './country-table.html',
   styleUrl: './country-table.css'
 })
 export class CountryTable {
   editid: any;
   countryData: any;
+  contactForm: any;
+  data: any;
   constructor(public api:Api, public router:Router,public modalservice:NgbModal){}
 
 
@@ -31,5 +35,33 @@ export class CountryTable {
       this.editid=id;
     }   
       this.modalservice.open(Modal,{centered: true, size:"lg", backdrop:'static'});
+  }
+  close(data:any=null){
+    this.editid = null;
+    this.modalservice.dismissAll();
+  }
+
+  async delete(id:any){
+    const result = await this.api.getMethod(`Master/DeleteCountryTable?ID=${id}`);
+    if (result.data > 0) {
+      Swal.fire({
+        title: "Are you sure want to delete?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        }
+      });
+      this.getData();
     }
+  }
+
 }
--- src/app/job-level-type-table/job-level-type-table.ts
+++ src/app/job-level-type-table/job-level-type-table.ts
@@ -7,7 +7,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-job-level-type-table',
-  imports: [SharedModule],
+  imports: [SharedModule, JobLevelTypeForm],
   templateUrl: './job-level-type-table.html',
   styleUrl: './job-level-type-table.css'
 })

b17a8f883535317d3f50774e7ba40ad1fdafeb40
Prasanna <prasannav2606@gmail.com>
2025-07-11 12:20:42 +0530

    Check

--- src/app/job-level-type-table/job-level-type-table.ts
+++ src/app/job-level-type-table/job-level-type-table.ts
@@ -14,7 +14,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class JobLevelTypeTable {
 jobLevelShift:any;
 
-constructor(public apiService: Api, public router: Router,  ){
+constructor(public apiService: Api, public router: Router,public modelService:NgbModal  ){
   
 }
 ngOnInit(){
@@ -31,7 +31,7 @@ addDetails(){
   this.router.navigate(['/jobleveltypeform'])
 }
 
-// openModal(modal: any){
-//   this.modalService.open(modal,{centered: true, size: "lg", scrollable: true})
-// }
+openModal(modal: any){
+  this.modelService.open(modal,{centered: true, size: "lg", scrollable: true})
+}
 }

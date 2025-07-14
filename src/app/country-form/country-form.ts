ebcb6bc19dd1917a16f83f454b225499369ddeee
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-14 17:20:35 +0530

    country table

--- src/app/country-form/country-form.ts
+++ src/app/country-form/country-form.ts
@@ -17,7 +17,6 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class CountryForm {
   @Input() editid: any;
   contactForm: any;
-  testvaraible:any;
   @Output() closeModal = new EventEmitter<any>();
   constructor(public api: Api, public router: Router, public activateRoute: ActivatedRoute, public modalservice: NgbModal) { }
   ngOnInit() {

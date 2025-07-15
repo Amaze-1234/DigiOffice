faffd52d8a6898cb9de54186b607f9632afe89b7
chethanaAmaze <chethana@amazeinc.in>
2025-07-15 12:24:36 +0530

    taking data sidebar and navbar

--- src/app/department-form/department-form.ts
+++ src/app/department-form/department-form.ts
@@ -39,11 +39,20 @@ export class DepartmentForm {
     }
     if (type == 'save') {
       const result = await this.api.postMethod('DigiOffice/InsertDepartment', this.departmentInfo.value);
-      this.closeModal.emit("save")
+      if (result.data > 0) {
+        Swal.fire("Data saved Successfully");
+        this.closeModal.emit("save")
+
+      }
     }
     else {
       const result = await this.api.postMethod('DigiOffice/UpdateDepartment', this.departmentInfo.value);
-      this.closeModal.emit("update")
+      if (result.data > 0) {
+        Swal.fire("Data Updated Successfully");
+        this.closeModal.emit("update")
+      }
+
+
     }
   }
   async updateDepartmentForm() {

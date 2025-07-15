9a3e2dfebb60b78c89da652d6de01ea1955e8616
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-15 17:34:50 +0530

    Shift

---
+++ src/Shared/search-pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+ transform(items: any[], searchText: string): any[] {
+    if (!items || !searchText) {
+      return items;
+    }
+
+    const lowerSearch = searchText.toLowerCase();
+
+    return items.filter(item => {
+      return Object.values(item).some(val =>
+        val?.toString().toLowerCase().includes(lowerSearch)
+      );
+    });
+  }
+
+
+
+}
--- src/Shared/shared.module.ts
+++ src/Shared/shared.module.ts
@@ -2,20 +2,23 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
+import { SearchPipe } from "./search-pipe";
 
 @NgModule({
     imports : [
         RouterOutlet,
         FormsModule,
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        SearchPipe
     ],
 
     exports : [
         RouterOutlet,
         FormsModule,
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        SearchPipe
     ]
 })
 

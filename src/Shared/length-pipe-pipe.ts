b3ccaf39918d2a1aafbb43ea30de333147dae67f
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-15 18:52:18 +0530

    dat saved

--- src/Shared/length-pipe-pipe.ts
+++ src/Shared/length-pipe-pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class LengthPipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(items: any[], searchText: string): any[] {
+    if (!items || !searchText) {
+      return items;
+    }
+
+    const lowerSearch = searchText.toLowerCase();
+
+    return items.filter(item => {
+      return Object.values(item).some(val =>
+        val?.toString().toLowerCase().includes(lowerSearch)
+      );
+    });
   }
 
 }

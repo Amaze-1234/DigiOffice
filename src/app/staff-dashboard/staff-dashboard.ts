bef771c82f76676b6c16873a040b684e1e778787
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-16 18:59:36 +0530

    dat saved

--- src/app/staff-dashboard/staff-dashboard.ts
+++ src/app/staff-dashboard/staff-dashboard.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedModule } from '../../Shared/shared.module';
 
 @Component({
   selector: 'app-staff-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule,SharedModule],
   templateUrl: './staff-dashboard.html',
   styleUrl: './staff-dashboard.css'
 })
 export class StaffDashboard {
+constructor(public router: Router){
 
+}
+navigateToEmployeeDetails(){
+  this.router.navigate(['/employee-details']);
+  console.log(1);
+  
+}
 }

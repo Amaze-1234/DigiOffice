550f81a865f28a98b2fa050f3829bb39ac6b32f9
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-16 14:05:30 +0530

    Sidebar

--- src/app/staff-navbar/staff-navbar.ts
+++ src/app/staff-navbar/staff-navbar.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SharedModule } from '../../Shared/shared.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-staff-navbar',
-  imports: [],
+  imports: [CommonModule,SharedModule],
   templateUrl: './staff-navbar.html',
   styleUrl: './staff-navbar.css'
 })
 export class StaffNavbar {
 
+    constructor(public router:Router){}
+  employee(){
+    this.router.navigate(['/employee-details']);   
+  }
+    position(){
+    this.router.navigate(['/position-details']);   
+  }
 }

f9728eebf62ca346050307969a7089f0cf87324f
Prasanna <prasannav2606@gmail.com>
2025-07-16 14:24:13 +0530

    Check

--- src/Shared/lenght-pipe-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LenghtPipePipe } from './lenght-pipe-pipe';
-
-describe('LenghtPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new LenghtPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});

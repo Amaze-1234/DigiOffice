cf62446fc436aebc486ecd72f61afe2bcba45ca6
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-17 10:08:35 +0530

    Employee Details

--- src/app/staff-dashboard/staff-dashboard.ts
+++ src/app/staff-dashboard/staff-dashboard.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedModule } from '../../Shared/shared.module';
+import { Loader } from '../../Services/loader';
 
 @Component({
   selector: 'app-staff-dashboard',
@@ -10,11 +11,13 @@ import { SharedModule } from '../../Shared/shared.module';
   styleUrl: './staff-dashboard.css'
 })
 export class StaffDashboard {
-constructor(public router: Router){
+constructor(public router: Router,public loaderService:Loader){
 
 }
 navigateToEmployeeDetails(){
   this.router.navigate(['/staff-details']);
+  sessionStorage.setItem('isDetail','employee');
+  this.loaderService.isDetail='employee';
   console.log(1);
   
 }

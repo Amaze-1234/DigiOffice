7c22fb27b548e1185dfdf33e1801ad092fb19800
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-17 15:14:30 +0530

    Sidebar

--- src/app/staff-dashboard/staff-dashboard.ts
+++ src/app/staff-dashboard/staff-dashboard.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedModule } from '../../Shared/shared.module';
 import { Loader } from '../../Services/loader';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Api } from '../../Services/api';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-staff-dashboard',
@@ -10,15 +13,44 @@ import { Loader } from '../../Services/loader';
   templateUrl: './staff-dashboard.html',
   styleUrl: './staff-dashboard.css'
 })
+
 export class StaffDashboard {
-constructor(public router: Router,public loaderService:Loader){
+
+   staffData: any;
+  searchText:any='';
+
+constructor(public router: Router,public loaderService:Loader,public apiservice: Api,public modalService: NgbModal){
 
 }
-navigateToEmployeeDetails(){
+
+
+
+  ngOnInit() {
+    this.getData();
+  }
+
+
+  async getData() {
+    const result = await this.apiservice.getMethod("Master/GetStaffDashboard");
+    this.staffData = result.data;
+
+
+  }
+  navigateToEmployeeDetails(){
   this.router.navigate(['/staff-details']);
   sessionStorage.setItem('isDetail','employee');
   this.loaderService.isDetail='employee';
   console.log(1);
   
 }
+
+
+
+// deleted(employeeID: number) {
+
+//   this.selectedEmployeeID = null;
+// }
+
+
+
 }

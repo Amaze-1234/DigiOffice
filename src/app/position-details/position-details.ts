d8449a662a0a1f8849402ffdf1a5c3194d095ae9
chethanaAmaze <chethana@amazeinc.in>
2025-07-18 16:09:25 +0530

    sending position details

--- src/app/position-details/position-details.ts
+++ src/app/position-details/position-details.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { SharedModule } from '../../Shared/shared.module';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Api } from '../../Services/api';
@@ -23,7 +23,7 @@ export class PositionDetails {
   provinceData: any;
   cityData: any;
   employeeDetails: any;
-    @Input() editid: any;
+  editid: any;
   unitType: any;
   department:any;
   unitValue: any;
@@ -41,7 +41,7 @@ export class PositionDetails {
     this.getProvince();
     this.getCity();
     this.getLoginDetails();
-    // this.getDepartment();
+    this.getDepartment()
     
   }
 
@@ -74,23 +74,21 @@ export class PositionDetails {
     this.departmentData = result.data;
 
   }
-  // async getDepartment() {
-  //   const result = await this.apiService.getMethod("DigiOffice/GetUnit");
-  //   this.department = result.data;
-    
+  async getDepartment() {
+    const result = await this.apiService.getMethod("DigiOffice/GetUnit");
+    this.department = result.data;
 
-  // }
+  }
 
-  async getDetails()
+  getDetails()
    {
-     const result = await this.apiService.getMethod(`Master/GetUnitByDepartment?ID=${this.positionDetails.value.DepartmentID}`);
-     this.department = result.data;
-  //  this.unitType = this.department
-  // .filter((x: { id: any }) => x.id == this.positionDetails.value.DepartmentID)
-  // .map((x: { unitName: any; id: any }) => ({
-  //   unitName: x.unitName,
-  //   departmentID: x.id
-  // }));
+
+   this.unitType = this.department
+  .filter((x: { id: any }) => x.id == this.positionDetails.value.DepartmentID)
+  .map((x: { unitName: any; id: any }) => ({
+    unitName: x.unitName,
+    departmentID: x.id
+  }));
    console.log(this.unitType)  
 }
  
@@ -144,12 +142,11 @@ export class PositionDetails {
     let response = await this.apiService.getMethod(`Master/GetPositionDetailsByEmployeeDetails?ID=${this.editid}`);
     console.log(response.data);
     this.positionDetails = new FormGroup({
-      DesignationID: new FormControl(response.data[0].designationID,Validators.required),
+      DesignationID: new FormControl(response.data[0].jobLevel,Validators.required),
       JobLevel: new FormControl(response.data[0].jobLevel, Validators.required),
       LoginType: new FormControl(response.data[0].loginType, Validators.required),
       DepartmentID: new FormControl(response.data[0].departmentID, Validators.required),
       UnitID: new FormControl(response.data[0].unitID, Validators.required),
-      Manager: new FormControl(response.data[0].manager,Validators.required),
       WorkArrangement: new FormControl(response.data[0].workArrangement, Validators.required),
       WorksiteCountry: new FormControl(response.data[0].worksiteCountry, Validators.required),
       WorksiteProvince: new FormControl(response.data[0].worksiteProvince, Validators.required),

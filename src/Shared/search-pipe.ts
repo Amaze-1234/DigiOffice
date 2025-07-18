8df2801120bb12f4222972f1787c635551418c4c
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-18 15:40:47 +0530

    Sidebar

--- src/Shared/search-pipe.ts
+++ src/Shared/search-pipe.ts
@@ -2,25 +2,39 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'search'
+  
 })
 export class SearchPipe implements PipeTransform {
   
 
- transform(items: any[], searchText: string): any[] {
-    if (!items || !searchText) {
-      return items;
-    }
+//  transform(items: any[], searchText: string): any[] {
+//     if (!items || !searchText) {
+//       return items;
+//     }
  
-    const lowerSearch = searchText.toLowerCase();
+//     const lowerSearch = searchText.toLowerCase();
  
+//     return items.filter(item => {
+//       return Object.values(item).some(val =>
+//         val?.toString().toLowerCase().includes(lowerSearch)
+//       );
+//     });
+//   }
+ transform(items: any[], searchText: string, designationId: string): any[] {
+    if (!items) return [];
+
+    const lowerSearch = searchText?.toLowerCase() || '';
+
     return items.filter(item => {
-      return Object.values(item).some(val =>
+      const matchesSearch = lowerSearch === '' || Object.values(item).some(val =>
         val?.toString().toLowerCase().includes(lowerSearch)
       );
+
+      const matchesDesignation = !designationId || item.designationID == designationId;
+
+      return matchesSearch && matchesDesignation;
     });
   }
-
-
 
 
 }

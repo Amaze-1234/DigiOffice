eff97ba136b7ef9d7e2cd85c50ff85cc68a00525
Prasanna <prasannav2606@gmail.com>
2025-07-18 11:05:17 +0530

    Check

--- src/app/staff-detail/staff-detail.ts
+++ src/app/staff-detail/staff-detail.ts
@@ -4,6 +4,7 @@ import { EmployeeDetails } from "../employee-details/employee-details";
 import { PositionDetails } from "../position-details/position-details";
 import { SharedModule } from '../../Shared/shared.module';
 import { Loader } from '../../Services/loader';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-staff-detail',
@@ -12,8 +13,17 @@ import { Loader } from '../../Services/loader';
   styleUrl: './staff-detail.css'
 })
 export class StaffDetail {
+  editid: any;
+ 
 
-  constructor(public loaderService:Loader){
+  constructor(public loaderService:Loader,public activateRoute:ActivatedRoute){
 
+  }
+  ngOnInit(){
+    
+    this.activateRoute.params.subscribe(parms=> {
+      debugger
+      this.editid = parms['id'];
+    })
   }
 }

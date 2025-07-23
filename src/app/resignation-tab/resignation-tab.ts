aa06c58a30f87978c1842e48bfef8f84ab73bdfe
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-23 10:34:18 +0530

    data saved

---
+++ src/app/resignation-tab/resignation-tab.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resignation-tab',
+  imports: [],
+  templateUrl: './resignation-tab.html',
+  styleUrl: './resignation-tab.css'
+})
+export class ResignationTab {
+
+}

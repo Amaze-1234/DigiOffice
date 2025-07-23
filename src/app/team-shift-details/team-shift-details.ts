30446bca6c50a8d689e61ee29ba53e4e297ede41
Amaze <venkatalakshmidesireddy@gmail.com>
2025-07-23 16:07:41 +0530

    data saved

---
+++ src/app/team-shift-details/team-shift-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-shift-details',
+  imports: [],
+  templateUrl: './team-shift-details.html',
+  styleUrl: './team-shift-details.css'
+})
+export class TeamShiftDetails {
+
+}

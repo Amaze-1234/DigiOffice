90e935708268aaeea0753efaf2d7495d3e0d7399
chethanaAmaze <chethana@amazeinc.in>
2025-08-01 15:13:48 +0530

    taking data

--- src/app/team-shift-details/team-shift-details.ts
+++ src/app/team-shift-details/team-shift-details.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../Shared/shared.module';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TeamShiftDetailsForm } from "../team-shift-details-form/team-shift-details-form";
 
 @Component({
   selector: 'app-team-shift-details',
-  imports: [SharedModule],
+  imports: [SharedModule, TeamShiftDetailsForm],
   templateUrl: './team-shift-details.html',
   styleUrl: './team-shift-details.css'
 })
 export class TeamShiftDetails {
 action:any;
-
+staffID:any;
+constructor(public modal:NgbModal){}
 myShift()
 {
  this.action='myShift';
@@ -18,6 +21,15 @@ myShift()
   myTeamShift()
   {
   this.action='myTeamShift';
+  }
+  openModal(id:any)
+  {
+       this.modal.open(id, { centered: true, size: "lg", backdrop: "static", scrollable: true });
+  }
+  close(id:any=null)
+  {
+   this.modal.dismissAll();
+   
   }
 
 }

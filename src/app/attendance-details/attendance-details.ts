2bafa227ae67ac4875cc389c61191c81cb2efc46
Amaze <venkatalakshmidesireddy@gmail.com>
2025-08-05 14:59:02 +0530

    leave-request

--- src/app/attendance-details/attendance-details.ts
+++ src/app/attendance-details/attendance-details.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../Shared/shared.module';
+import { Api } from '../../Services/api';
 
 @Component({
   selector: 'app-attendance-details',
@@ -7,6 +8,16 @@ import { SharedModule } from '../../Shared/shared.module';
   templateUrl: './attendance-details.html',
   styleUrl: './attendance-details.css'
 })
-export class AttendanceDetails {
+export class AttendanceDetails implements OnInit {
+attendanceDetails:any;
+  constructor(public apiservice: Api) { }
+  ngOnInit() {
+    this.getData();
 
+  }
+  async getData(){
+    const result=await this.apiservice.getMethod("Master/GetAttendanceDetailsStaff");
+    this.attendanceDetails=result.data;
+    console.log(this.attendanceDetails);
+  }
 }
